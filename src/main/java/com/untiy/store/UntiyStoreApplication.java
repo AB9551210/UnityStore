93af7f74fd9100d552e1b2db5d825658c8d668ca
Ab18778041017 <1119371702@qq.com>
2021-12-03 22:01:08 +0800

    Initial commit

---
+++ src/main/java/com/untiy/store/UntiyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.untiy.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntiyStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntiyStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/untiy/store/UntiyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.untiy.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntiyStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
